9bb71e4c0752d7e28651518aff3ecbf1d7713f29
Vishalgitdemo <vishalpratapsingh143047@gmail.com>
2025-06-23 00:52:11 +0530

    some basic contant

--- Hello.py
+++ Hello.py
@@ -5,3 +5,14 @@ name="vashu" # variable= vary ho skta hai
 env="dev" #dev=constant ,env=variable
 print("Hello",name)
 
+# int, float, str
+int- integer - -inf ,.......-1,0,1, ..inf
+float - decimal  -0.1,0.01,-0.1,10.3
+str - charaters - "a",'a',"bus","im in a bus ","","#","@"
+
+
+# Ctrl + A (Select All)
+# Ctrl + / (Comment All)
+ # Varialbles 
+
+ # 
