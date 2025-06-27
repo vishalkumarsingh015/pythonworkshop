a23f55847c6084db99861904cac53a3a66a08982
Vishalgitdemo <vishalpratapsingh143047@gmail.com>
2025-06-27 11:48:13 +0530

    condit

---
+++ 08_If_else.py
@@ -0,0 +1,79 @@
+#
+
+# if True:
+#     print('Hey it is True')
+# else:
+#     print('Hey it is False')
+
+# print(10>8)
+# print(10<8)
+# print(10==8)
+# print(10==10)
+
+# if 10<8:
+#     print('this is true')
+# else:
+#     print('This is false')
+
+
+# print('Enter a no to check if that is EVEN or Not')
+# num =int(input('Enter a no -'))
+# if num % 2  ==0:
+#     print('Yes no is Even')
+# else:
+#     print('Yes no is Odd')    
+
+# users=['Vishal','Rajesh','Ankit']
+# if 'Vishal' in users:
+#   print('User is Exist')
+
+# else:
+#   print('User does not  Exist')
+
+# marks= int(input('Enter total marks:'))
+
+# if marks>=88:
+#   print('A Grade')
+# elif marks>=66:
+#   print('B Grade')  
+# elif marks>=40:
+#  print('C Grade')
+# else:
+#   print('Failed !!!')
+
+# -------------------------------
+# Logical Operation 
+
+# Condition1 and  condition2
+# If both conditions are true then true else False
+
+
+# condition1 or condition2
+
+# if any of the condition is true then true
+Age =20
+vot_id=True
+
+
+if Age>=18 and vot_id:
+  print('you can vote')
+
+else:
+  print('you cont vote')
+
+
+
+  # or condition
+
+
+
+Age =20
+vot_id=False
+
+
+if Age>=18 or vot_id:
+  print('you can vote')
+
+else:
+  print('you cont vote')
+
